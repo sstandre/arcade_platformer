2c7763323daaa006efe87f867c61f1a5b5335c59
sstandre <simon@saintandre.com.ar>
2021-05-02 09:53:01 -0300

    define variables in init

--- arcade_platformer/main.py
+++ arcade_platformer/main.py
@@ -9,10 +9,51 @@ Game sounds from: ...
 """
 
 import arcade
+import pathlib
+
+# Game constant
+# Window dimensions
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Arcade Platformer"
+
+# Assets path
+ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
 
 class Platformer(arcade.Window):
     def __init__(self):
-        pass
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        
+        # These lists will hold different sets of sprites
+        self.coins = None
+        self.bakcground = None
+        self.walls = None
+        self.ladders = None
+        self.goals = None
+        self.enemies = None
+
+        # One sprite for the player, no more is needed
+        self.player = None
+
+        # We need a physics engine as well
+        self.physics_engine = None
+
+        # Someplace to keep score
+        self.score = 0
+
+        # Which level are we on?
+        self.level = 1
+
+        # Load up our sounds here
+        self.coin_sound = arcade.load_sound(
+            str(ASSETS_PATH / "sounds" / "coin.wav")
+            )
+        self.jump_sound = arcade.load_sound(
+            str(ASSETS_PATH / "sounds" / "jump.wav")
+            )
+        self.victory_sound = arcade.load_sound(
+            str(ASSETS_PATH / "sounds" / "victory.wav")
+            )
 
     def setup(self):
         """Sets up thegame for the current level"""
@@ -25,6 +66,7 @@ class Platformer(arcade.Window):
             key {int} -- Which key was pressed
             modifiers {int} -- Which modifiers were down at the time
         """
+        pass
 
     def on_key_release(self, key, modifiers):
         """Process key release
@@ -33,7 +75,8 @@ class Platformer(arcade.Window):
             key {int} -- Which key was released
             modifiers {int} -- Which modifiers were down at the time
         """
-
+        pass
+    
     def on_update(self, delta_time):
         """Updates the position of all game objects
 
