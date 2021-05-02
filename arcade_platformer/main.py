76b173c032f539b6fec84be2d4a56902fdd9fd7b
sstandre <simon@saintandre.com.ar>
2021-05-02 11:23:35 -0300

    add level data in setup

--- arcade_platformer/main.py
+++ arcade_platformer/main.py
@@ -17,6 +17,14 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Arcade Platformer"
 
+# Scaling constants
+MAP_SCALING = 1.0
+
+# Player constants
+GRAVITY = 1.0
+PLAYER_START_X = 65
+PLAYER_START_Y = 256
+
 # Assets path
 ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
 
@@ -54,10 +62,65 @@ class Platformer(arcade.Window):
         self.victory_sound = arcade.load_sound(
             str(ASSETS_PATH / "sounds" / "victory.wav")
             )
-
+ 
     def setup(self):
         """Sets up thegame for the current level"""
-        pass
+
+        # Get the current map based on the level
+        map_name = f"platform_level_{self.level:02}.tmx"
+        map_path = ASSETS_PATH / map_name
+
+        # What are the name of the layers?
+        wall_layer = "ground"
+        coin_layer = "coins"
+        goal_layer = "goal"
+        background_layer = "background"
+        ladders_layer = "ladders"
+
+        # Load the current map
+        game_map = arcade.tilemap.read_tmx(str(map_path))
+
+        #Load the layers
+        self.background = arcade.tilemap.process_layer(
+            game_map, layer_name=background_layer, scaling=MAP_SCALING
+        )
+        self.goals = arcade.tilemap.process_layer(
+            game_map, layer_name=goal_layer, scaling=MAP_SCALING
+        )
+        self.walls = arcade.tilemap.process_layer(
+            game_map, layer_name=wall_layer, scaling=MAP_SCALING
+        )
+        self.ladders = arcade.tilemap.process_layer(
+            game_map, layer_name=ladders_layer, scaling=MAP_SCALING
+        )
+        self.coins = arcade.tilemap.process_layer(
+            game_map, layer_name=coin_layer, scaling=MAP_SCALING
+        )
+
+        # Set the background color
+        if game_map.background_color is None:
+            background_color = arcade.color.FRESH_AIR
+        else:
+            background_color = game_map.background_color
+        arcade.set_background_color(background_color)
+
+        # Create the player sprite if they're not already set up
+        if self.player is None:
+            self.player = self.create_player_sprite()
+
+        # Move the player sprite to the beginning
+        self.player.center_x = PLAYER_START_X
+        self.player.center_y = PLAYER_START_Y
+        self.player.change_x = 0
+        self.player.change_y = 0
+
+        # Load the physiscs engine for this map
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            player_sprite=self.player,
+            platforms=self.walls,
+            gravity_constant=GRAVITY,
+            ladders=self.ladders,
+        )
 
     def on_key_press(self, key, modifiers):
         """Process key presses
