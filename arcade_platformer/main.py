48707a06cbf553fbc2cfbff666d9e52865100359
sstandre <simon@saintandre.com.ar>
2021-05-02 11:51:07 -0300

    create player sprite

--- arcade_platformer/main.py
+++ arcade_platformer/main.py
@@ -122,6 +122,61 @@ class Platformer(arcade.Window):
             ladders=self.ladders,
         )
 
+    def create_player_sprite(self):
+        """Creates the animated player sprite
+
+        Returns :
+            the properly set up player sprite
+        """
+        # Where are the player images stored?
+        texture_path = ASSETS_PATH / "images" / "player"
+
+        # Set up the apporpiate textures
+        walking_paths = [
+            texture_path / f"alienGreen_walk{x}.png" for x in (1,2)
+        ]
+        climbing_paths = [
+            texture_path / f"alienGreen_climb{x}.png" for x in (1,2)
+        ]
+        stanging_path = texture_path / "alienGreen_stand.png"
+        
+        # Load them all now
+        walking_right_textures = [
+            arcade.load_texture(tex) for tex in walking_paths
+        ]
+        walking_left_textures = [
+            arcade.load_texture(tex, mirrored=True) for tex in walking_paths
+        ]
+        walking_up_textures = [
+            arcade.load_texture(tex) for tex in climbing_paths
+        ]
+        walking_down_textures = [
+            arcade.load_texture(tex) for tex in climbing_paths
+        ]
+        standing_right_textures = [arcade.load_texture(stanging_path)]
+        standing_left_textures = [arcade.load_texture(stanging_path, mirrored=True)]
+        
+        # Create the sprite
+        player = arcade.AnimatedWalkingSprite()
+
+        # Add the proper textures
+        player.stand_left_textures = standing_left_textures
+        player.stand_right_textures = standing_right_textures
+        player.walk_left_textures = walking_left_textures
+        player.walk_right_textures = walking_right_textures
+        player.walk_up_textures = walking_up_textures
+        player.walk_down_textures = walking_down_textures
+
+        # Set the player defaults
+        player.center_x = PLAYER_START_X
+        player.center_y = PLAYER_START_Y
+        player.state = arcade.FACE_RIGHT
+
+        # Set the initial texture
+        player.texture = player.stand_right_textures[0]
+
+        return player
+
     def on_key_press(self, key, modifiers):
         """Process key presses
 
